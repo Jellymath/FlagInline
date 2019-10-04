257c9c2f0d9067300f511313801e2aee92443bbc
Alexander Jellymath <Jellymath@outlook.com>
2019-10-05 01:53:04 +0300

    Provide possibility to create empty and all values set, do initial function extraction. NB: KotlinPoet infix function handling not always works as expected, replaced to simple calls for now

--- flag-inline-generator/src/main/kotlin/edu/jellymath/FlagsGenerator.kt
+++ flag-inline-generator/src/main/kotlin/edu/jellymath/FlagsGenerator.kt
@@ -11,20 +11,18 @@ import javax.lang.model.element.Element
 import javax.lang.model.element.ElementKind
 import javax.lang.model.element.TypeElement
 
+private const val KAPT_KOTLIN_GENERATED_OPTION_NAME = "kapt.kotlin.generated"
+
 @AutoService(Processor::class)
 class FlagsGenerator : AbstractProcessor() {
-    override fun getSupportedAnnotationTypes(): Set<String> = setOf(Flags::class.java.name)
-
+    override fun getSupportedAnnotationTypes(): Set<String> = setOf(name<Flags>())
     override fun getSupportedSourceVersion(): SourceVersion = SourceVersion.latest()
 
-    override fun process(annotations: MutableSet<out TypeElement>, roundEnv: RoundEnvironment): Boolean {
-        println("process")
-        roundEnv.getElementsAnnotatedWith(Flags::class.java)
+    override fun process(annotations: Set<TypeElement>, roundEnv: RoundEnvironment): Boolean {
+        roundEnv.getElementsAnnotatedWith<Flags>()
             .filter { it.kind == ElementKind.ENUM }
             .forEach {
-
                 val className = it.simpleName.toString()
-                println("Processing: $className")
                 val pack = processingEnv.elementUtils.getPackageOf(it).toString()
                 generateClass(className, pack, it.enclosedElements.filter { it.kind == ElementKind.ENUM_CONSTANT })
             }
@@ -33,48 +31,20 @@ class FlagsGenerator : AbstractProcessor() {
 
     private fun generateClass(
         originalClassName: String,
-        pack: String,
+        packageName: String,
         enclosedElements: List<Element>
     ) {
-        val className = "Inline$originalClassName"
-        val type = ClassName(pack, className)
-        val setName = "${className}Set"
-        val setType = ClassName(pack, setName)
-        val fileSpec = buildFile(pack, className) {
-            clazz(className) {
-                inline
-                primaryConstructor {
-                    parameter<UInt>("flag")
-                }
-                property<UInt>("flag") {
-                    initializer("flag")
-                }
-                companionObject {
-                    var current = 1u
-                    for (el in enclosedElements) {
-                        property(el.simpleName.toString(), type) {
-                            initializer("$className(%L)", "${current}u")
-                        }
-                        current = current shl 1
-                    }
-                }
-            }
+        val name = "Inline$originalClassName"
+        val type = ClassName(packageName, name)
+        val setName = "${name}Set"
+        val setType = ClassName(packageName, setName)
+        val allValuesMask = generateSequence(1u) { (it shl 1) + 1u }.elementAt(enclosedElements.lastIndex)
 
-            clazz(setName) {
-                inline
-                primaryConstructor {
-                    parameter<UInt>("set")
-                }
-                property<UInt>("set") {
-                    initializer("set")
-                }
-            }
+        val fileSpec = buildFile(packageName, name) {
+            mainClass(name, enclosedElements, type)
+            setClass(setName)
 
-            function("toSet") {
-                receiver(type)
-                returns(setType)
-                addStatement("return $setName(flag)")
-            }
+            creationFunctions(type, setType, setName, packageName, allValuesMask)
 
             function("contains") {
                 operator
@@ -97,27 +67,11 @@ class FlagsGenerator : AbstractProcessor() {
                 receiver(setType)
                 parameter("other", type)
                 returns(setType)
-                addStatement("return this + other")
+                addStatement("return $setName(set or other.flag)")
             }
 
             function("or") {
                 infix
-                receiver(setType)
-                parameter("other", setType)
-                returns(setType)
-                addStatement("return this + other")
-            }
-
-            function("plus") {
-                operator
-                receiver(setType)
-                parameter("other", type)
-                returns(setType)
-                addStatement("return $setName(set or other.flag)")
-            }
-
-            function("plus") {
-                operator
                 receiver(setType)
                 parameter("other", setType)
                 returns(setType)
@@ -131,13 +85,96 @@ class FlagsGenerator : AbstractProcessor() {
                 returns(setType)
                 addStatement("return $setName(set and other.set)")
             }
+
+            arithmeticFunctions(setType, type)
         }
 
         val kaptKotlinGeneratedDir = processingEnv.options.getValue(KAPT_KOTLIN_GENERATED_OPTION_NAME)
         fileSpec.writeTo(File(kaptKotlinGeneratedDir))
     }
 
-    companion object {
-        const val KAPT_KOTLIN_GENERATED_OPTION_NAME = "kapt.kotlin.generated"
+    private fun FileSpec.Builder.creationFunctions(
+        type: ClassName,
+        setType: ClassName,
+        setName: String,
+        packageName: String,
+        allValuesMask: UInt
+    ) {
+        val companionType = ClassName(packageName, "$setName.Companion")
+
+        function("toSet") {
+            receiver(type)
+            returns(setType)
+            addStatement("return $setName(flag)")
+        }
+
+        function("emptySet") {
+            receiver(companionType)
+            returns(setType)
+            addStatement("return $setName(0u)")
+        }
+
+        function("allValues") {
+            receiver(companionType)
+            returns(setType)
+            addStatement("return $setName(${allValuesMask.hexLiteral()})")
+        }
     }
+
+    private fun FileSpec.Builder.mainClass(name: String, enclosedElements: List<Element>, type: ClassName) {
+        clazz(name) {
+            inline
+            primaryConstructor {
+                parameter<UInt>("flag")
+            }
+            property<UInt>("flag") {
+                initializer("flag")
+            }
+            companionObject {
+                var current = 1u
+                for (el in enclosedElements) {
+                    property(el.simpleName.toString(), type) {
+                        initializer("$name(${current.hexLiteral()})")
+                    }
+                    current = current shl 1
+                }
+            }
+        }
+    }
+
+    private fun FileSpec.Builder.setClass(name: String) {
+        clazz(name) {
+            inline
+            primaryConstructor {
+                parameter<UInt>("set")
+            }
+            property<UInt>("set") {
+                initializer("set")
+            }
+            companionObject()
+        }
+    }
+
+    private fun FileSpec.Builder.arithmeticFunctions(setType: ClassName, type: ClassName) {
+        function("plus") {
+            operator
+            receiver(setType)
+            parameter("other", type)
+            returns(setType)
+            addStatement("return this.or(other)")
+        }
+
+        function("plus") {
+            operator
+            receiver(setType)
+            parameter("other", setType)
+            returns(setType)
+            addStatement("return this.or(other)")
+        }
+    }
+
+    private fun UInt.hexLiteral(): String = "0x${toString(16)}u"
+    private inline fun <reified T> name() = T::class.java.name
+    private inline fun <reified T : Annotation> RoundEnvironment.getElementsAnnotatedWith() =
+        getElementsAnnotatedWith(T::class.java)
 }
